8f5bfd0b6a1a75f61ae332792b810b96e02ce395
Pablo Pernias <pablo@pernias.com>
2016-06-20 17:13:06 +0200

    lot of refactor

--- index.js
+++ index.js
@@ -3,14 +3,48 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
 http.listen(port, function(){
-  console.log("webserver listening on *:${port}");
+  console.log(`webserver listening on *:${port}`);
 });
 
+const auctions = [];
+
+const items = [
+  {
+    name: 'Bike',
+    picture: 'https://upload.wikimedia.org/wikipedia/commons/c/c6/A_mountain_bike_styled_e-bike,_Cyclotricity_Stealth.jpg',
+    description: 'This awesome bike is very awesome, and it can fly (not really)...'
+  },
+  {
+    name: '1.000.000 $',
+    picture: 'https://consumermediallc.files.wordpress.com/2012/06/heapocash.jpg',
+    descriotion: 'Wow, that sure is a lot of money....'
+  },
+  {
+    name: 'Funny & cute dead squirrel',
+    picture: 'https://i.ytimg.com/vi/ICE9Qe8HrqE/hqdefault.jpg',
+    descriotion: 'Kids love it, adults love it, you will love it too!!'
+  },
+  {
+    name: 'Slave',
+    picture: 'http://cdn.modernghana.com/images/content/slavesscar65.jpg',
+    descriotion: 'We all need a helping hand sometimes!'
+  }
+]
+
+//initialize all auctions
+for(var i = 0; i < 4 ; i++){
+  var d = new Date();
+  d.setMinutes(d.getMinutes() + 5);
+  auctions.push({
+    item: items[i],
+    endDate: d
+  });
+}
 
 // io.on('connection', function(socket){
 //   console.log('a user connected');
@@ -24,77 +58,13 @@ http.listen(port, function(){
 //     console.log('user disconnected');
 //   });
 // });
-//
-const initialCountdown = 30;
-var countdown = initialCountdown;
-var numbers = {};
-
-setInterval(function() {
-  countdown--;
-  io.sockets.emit('timer', { countdown: countdown });
-
-  if(countdown <= 0){
-
-    countdown = initialCountdown;
-
-    var winner;
-
-    for(var number in numbers){
-
-      if(numbers[number].length == 1){
-        if(!winner || winner > number ){
-          winner = number;
-
-        }
-      }
-
-    }
-
-    if(winner){
-      io.sockets.emit('winner', {name: numbers[winner][0], number: winner })
-    }
-
-    numbers = {}
-
-  }
-}, 1000);
-
 
 io.sockets.on('connection', function (socket) {
-  socket.on('reset', function (data) {
-    countdown = initialCountdown;
-    io.sockets.emit('timer', { countdown: countdown });
-  });
-
-  socket.on('enter_number', function(message){
-
-
-
-   console.log('got message ${message}, broadcasting to all');
-
-                        if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
-                          //  return res.json({"responseCode" : 1,"responseDesc" : "Please select captcha"});
-                          }
-                          // Put your secret key here.
-                          var secretKey = "6LcQ2B4TAAAAANjWiObVOTBz1eQo67omwxUhwiuU";
-                          // req.connection.remoteAddress will provide IP address of connected user.
-                          var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
-                          // Hitting GET request to the URL, Google will respond with success or error scenario.
-                          request(verificationUrl,function(error,response,body) {
-                            body = JSON.parse(body);
-                            // Success will be true or false depending upon captcha validation.
-                            if(body.success !== undefined && !body.success) {
-                              return res.json({"responseCode" : 1,"responseDesc" : "Failed captcha verification"});
-                            }else{
-                              io.emit('enter_number', message.number);
-                              numbers[message.number] = numbers[message.number] ? numbers[message.number].push(message.name) :[message.name];
-                              console.log(JSON.stringify(numbers));
-                            }
-                            res.json({"responseCode" : 0,"responseDesc" : "Sucess"});
-                          });
-
+  //We do not keep track of users or connections...
+  socket.on('signin', function(data){
 
   });
+
 });
 
 // export app so we can test it
